0c15b836cc3d1d6b721b49beceb43a1639a4deaf
QACAMYRAI <86566403+QACAMYRAI@users.noreply.github.com>
2021-07-02 10:32:31 +0300

    New

--- jira api.py
+++ jira api.py
@@ -1,14 +1,11 @@
 from jira import JIRA
-import requests
-from requests.auth import HTTPBasicAuth
-import json
+
 token = 'MTg2NTgxMDI4Mzg5OnXd8RaxXO9i4waU5vxJvcE0M5xK'
 username = r"bytex"
 base_url = r"https://jira.fraglab.com"
 
-#session = requests.session()
-#resp = session.get(base_url + "/browse/LI-4240", auth=HTTPBasicAuth(username, token))
-#nedded = requests.get(r"https://jira.fraglab.com", auth=('bytex', '3b7cbc8bbe0c7866ccd003c1ba21add68f6ddb31'))
-#print(nedded.text)
-response = requests.get("https://jira.fraglab.com//rest//api//latest//issue//LI-3981", auth=(username, token))
-print(response.text)
+jira_options = {'server': 'https://jira.fraglab.com'}
+jira = JIRA(options=jira_options, basic_auth=(r'bytex', 'MTg2NTgxMDI4Mzg5OnXd8RaxXO9i4waU5vxJvcE0M5xK'))
+#jql = 'project = ' + "lI-3891"
+#issues_list = jira.search_issues(jql)
+issue = jira.issue("LI-3891")
